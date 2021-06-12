b2cf97d2512c647eed9651d18ecc133513a3ec4d
James <adewale.j.dada@gmail.com>
2021-06-12 18:47:14 -0400

    Resets database

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,8 @@
 # Adding this becaause it will not commit otherwise
 
 Character.create([
-    {name: "Neo", description: false},
-    {name: "Trinity", description: false},
-    {name: "Morpheus", description: false},
-    {name: "Agent Smith", description: false}
+    {name: "Neo", description: "a fictional character and the protagonist of The Matrix franchise"},
+    {name: "Trinity", description: "a computer programmer and a hacker who has escaped from the Matrix, a sophisticated computer program where most humans are imprisoned."},
+    {name: "Morpheus", description: "The name Morpheus is that of the god of dreams in Greek mythology, which is consistent with the character's involvement with the dreaming of the Matrix"},
+    {name: "Agent Smith", description: "n Agent, an AI program in the Matrix programmed to keep order within the system by terminating human simulacra which would bring instability to the simulated reality"}
 ])
